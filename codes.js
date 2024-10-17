1e1ba3ce3b7eb3f58a06fa8c172606d244b25808
Ragunatan <ddonnatan@gmail.com>
2024-10-17 10:49:04 +0530

    added loacl storage and display on page

--- codes.js
+++ codes.js
@@ -3,6 +3,13 @@ function store(){
     let usernameData = document.getElementById('username').value
     let PhoneData = document.getElementById('phoneNumber').value
 
+let user = document.querySelector('.userData');
+let listItem = document.createElement('li')
+listItem.textContent = emailData + '-' + usernameData + '-' + PhoneData
+
+user.appendChild(listItem)
+
+
 localStorage.setItem('email',emailData)
 localStorage.setItem('userName',usernameData)
 localStorage.setItem('phone',PhoneData)
